dfe79bc185d58985d9c8d987c66e61940543db04
Paul BIE <pbie@e3r6p9.42.fr>
2019-11-20 15:00:53 +0100

    [Add] - Parsing

--- src/parse.js
+++ src/parse.js
@@ -1,15 +1,66 @@
+function isNum(str) {
+	return /^\d+$/.test(str);
+}
+
+function isAlpha(str) {
+	return /^[a-zA-Z]+$/.test(str);
+}
+
+function handlePower(powerPiece) {
+	const [letter, power] = powerPiece.split('^');
+	if (!power) {
+		if (powerPiece.length === 1 && isAlpha(powerPiece)) return true;
+		console.log('Invalid Structure');
+		return false;
+	}
+	if (!isAlpha(letter) || letter.length > 1) {
+		console.log('Invalid Variable');
+		return false;
+	}
+	if (!isNum(power)) {
+		console.log('Invalid Power');
+		return false;
+	}
+	if (power > 2) {
+		console.log('Degree greater than 2');
+		return false;
+	}
+	return true;
+}
+
 function parse(equation) {
+	let variable = null;
+	let equals = 0;
 	let x = -1;
-	let valid = true;
-	while (++x <= equation.length) {
-		if (x === 0) {
-			if (equation[x].match(/[a-z]/i)) {
-				valid === false;
-				break;
+	while (++x < equation.length) {
+		if (x % 2 === 0) {
+			if (isNaN(equation[x])) {
+				if (!handlePower(equation[x])) return false;
+				else {
+					const newVar = equation[x][0];
+					if (!variable) variable = newVar;
+					else if (newVar !== variable) {
+						console.log('Only one variable allowed');
+						return false;
+					}
+				}
 			}
+		} else {
+			if (
+				equation[x] != '*' &&
+				equation[x] != '+' &&
+				equation[x] != '-' &&
+				equation[x] != '/' &&
+				equation[x] != '='
+			) {
+				console.log('invalid operator', equation[x]);
+				return false;
+			}
+			if (equation[x] === '=') equals++;
+			if (equals > 1) return false;
 		}
 	}
-	return valid;
+	return true;
 }
 
 module.exports = {
