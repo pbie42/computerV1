52a1a595537a93ea0a26883528b398b66c83d72c
Paul BIE <pbie@e3r9p11.42.fr>
2019-11-21 13:41:07 +0100

    [Wip] - Reduce

--- src/reduce.js
+++ src/reduce.js
@@ -1,13 +1,17 @@
-function isOperator(op) {
-	if (op === '+' || op === '-' || op === '*') return true;
+function isValidOperator(op) {
+	if (op === '+' || op === '-') return true;
 	return false;
 }
 
-function reduceEquation(equation) {
+function reduceEquation(eq, commons) {
 	let x = 0;
-	while (x < equation.length) {
+	while (x < eq.length) {
 		if (x % 2 === 0) {
-			if (!isNaN(equation[x])) {
+			if (!isNaN(eq[x])) {
+				if (eq[x + 1] && isValidOperator(eq[x + 1])) {
+					if (eq[x - 1] && eq[x - 1] === '-') {
+					}
+				}
 			}
 		}
 		x++;
